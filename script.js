b2f0faf35ca706010aac6fd7ed905b074913a1ab
sss <matusoyo2436@gmail.com>
2024-09-04 23:44:17 +0900

    Add more text

--- script.js
+++ script.js
@@ -9,4 +9,13 @@ box.addEventListener('mousedown', function(e) {
         box.offsetLeft - e.clientX,
         box.offsetTop - e.clientY
     ]
-},true)
+}, true)
+
+document.addEventListener('mouseup', function() {
+    isDown = false
+    box.classList.remove('active')
+}, true)
+
+document.addEventListener('mouseup', function(e) {
+
+}, true)
